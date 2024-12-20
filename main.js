bd1830d0c32855eb5109e560493e86fef02c0388
Gabriella Calvano <gabcalvano@gmail.com>
2024-12-20 09:06:58 +0100

    completed html structure

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+/* 
+1 - El programa genera un movimiento al azar (piedra, papel o tijera)
+2 - la usuaria juega
+3 - El programa compara los movimientos y decide si la usuaria ha ganado, perdido o empatado.
+4 - El programa conta el numero de punto del jugador y del ordenador.
+5 - El juego acaba cuando se realizan 10 movimientos.
+*/
+
+/*
+1. - Crear el html (main, h1, h2,  form, input selector(3 opciones), boton jugar, p(resultado), )
+2. - Crear variables - selecionar los elementos que interactuan con el programa
+3. - Generar un número aleatorio entre 1 y 9
+4. - Generar un movimiento aleatorio
+5. - Comparar el movimiento que la jugadora ha seleccionado con el movimiento que ha generado la
+computadora y pintar las pistas correspondientes en la pantalla.
+*/
